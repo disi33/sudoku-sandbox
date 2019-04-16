ffe7e115b35f44fe3b09739c6af60dc6dabd4120
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2019-04-16 13:35:03 +0100

    Fix Cell stories

--- src/components/Cell/Cell.stories.js
+++ src/components/Cell/Cell.stories.js
@@ -7,12 +7,12 @@ import './Cell.css';
 const defaultCellProps = {
     coords: {row: 0, col: 0},
     value: undefined,
-    borders: [],
+    borders: ['u', 'r', 'd', 'l'],
     candidates: [],
     pencilMarks: [],
 }
 
-const allBorders = ['u', 'r', 'd', 'l'];
+const allThickBorders = ['U', 'R', 'D', 'L'];
 const someCandidates = [1, 2, 3];
 const somePencilMarks = [1, 2, 3, 4, 5, 6, 7, 8];
 
@@ -20,6 +20,6 @@ storiesOf('Cell', module)
     .addDecorator(story => <div style={{padding: '24px'}}>{story()}</div>)
     .add('empty', () => <Cell {...defaultCellProps}></Cell>)
     .add('with value', () => <Cell {...defaultCellProps} value={3}></Cell>)
-    .add('with borders', () => <Cell {...defaultCellProps} borders={allBorders}></Cell>)
+    .add('with thick borders', () => <Cell {...defaultCellProps} borders={allThickBorders}></Cell>)
     .add('with candidates', () => <Cell {...defaultCellProps} candidates={someCandidates}></Cell>)
     .add('with pencil marks', () => <Cell {...defaultCellProps} pencilMarks={somePencilMarks}></Cell>);
