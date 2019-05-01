6eb1d30ed42948cbe91ddc5cf9533b5267c4bf34
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2019-05-02 00:42:04 +0100

    Move to new Firebase domain

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,11 +3,11 @@ import 'firebase/storage';
 
 const config = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: "pseudoku-5aa2b.firebaseapp.com",
-    databaseURL: "https://pseudoku-5aa2b.firebaseio.com",
-    projectId: "pseudoku-5aa2b",
-    storageBucket: "pseudoku-5aa2b.appspot.com",
-    messagingSenderId: "212682065094"
+    authDomain: "pseudoku-app.firebaseapp.com",
+    databaseURL: "https://pseudoku-app.firebaseio.com",
+    projectId: "pseudoku-app",
+    storageBucket: "pseudoku-app.appspot.com",
+    messagingSenderId: "517417182245"
 };
 
 export default firebase.initializeApp(config);
