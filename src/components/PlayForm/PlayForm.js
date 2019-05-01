9c931d402ae5dc648f1c3dc18d6674e91ed759f3
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2019-05-01 13:17:38 +0100

    Added a timer

--- src/components/PlayForm/PlayForm.js
+++ src/components/PlayForm/PlayForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useStopwatch } from 'react-timer-hook';
 
 import ColorPicker from '../ColorPicker/ColorPicker';
 
@@ -8,6 +9,7 @@ import '../EditForm/EditForm.css';
 export default function PlayForm({onStartOver, canUndo, onUndo, canRedo, onRedo, onColorSelected}) {
 
     const [startOverActive, setStartOverActive] = useState(false);
+    const { hours, minutes, seconds, start, pause, reset } = useStopwatch();
 
     return (
         <div className="edit-form play-form">
@@ -30,15 +32,35 @@ export default function PlayForm({onStartOver, canUndo, onUndo, canRedo, onRedo,
                 </div>
                 <div className="edit-form__field">
                     <div className="edit-form__field-input play-form__undo-buttons">
-                        <button className={"play-form__undo-redo " + (canUndo ? '' : 'play-form__undo-redo--disabled')} disabled={!canUndo} onClick={() => onUndo()}>Undo</button>
-                        <button className={"play-form__undo-redo " + (canRedo ? '' : 'play-form__undo-redo--disabled')} disabled={!canRedo} onClick={() => onRedo()}>Redo</button>
+                        <button className={"play-form__button play-form__undo-redo " + (canUndo ? '' : 'play-form__undo-redo--disabled')} disabled={!canUndo} onClick={() => onUndo()}>Undo</button>
+                        <button className={"play-form__button play-form__undo-redo " + (canRedo ? '' : 'play-form__undo-redo--disabled')} disabled={!canRedo} onClick={() => onRedo()}>Redo</button>
                     </div>
                 </div>
                 <div className="edit-form__field">
-                    {!startOverActive && <button className="play-form__start-over" onClick={() => setStartOverActive(true)}>Start over!</button>}
-                    {startOverActive && <button className="play-form__start-over play-form__start-over--danger" onClick={() => onStartOver()} onMouseLeave={() => setStartOverActive(false)}>Really start over?</button>}
+                    {!startOverActive && <button className="play-form__button play-form__start-over" onClick={() => setStartOverActive(true)}>Start over!</button>}
+                    {startOverActive && <button className="play-form__button play-form__start-over play-form__start-over--danger" onClick={() => onStartOver()} onMouseLeave={() => setStartOverActive(false)}>Really start over?</button>}
+                </div>
+            </div>
+            <div className="edit-form__section">
+                <div className="edit-form__section-title">Timer</div>
+                <div className="play-form__timer">{timerString(hours, minutes, seconds)}</div>
+                <div className="edit-form__field">
+                    <div className="edit-form__field-input play-form__timer-buttons">
+                        <button className="play-form__button" onClick={() => start()}>Start</button>
+                        <button className="play-form__button" onClick={() => pause()}>Stop</button>
+                        <button className="play-form__button" onClick={() => reset()}>Clear</button>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
+
+
+const timerString = (hours, minutes, seconds) => {
+    const paddedSeconds = seconds.toString().padStart(2, '0');
+    const paddedMinutes = minutes.toString().padStart(2, '0');
+    const paddedHours = hours === 0 ? '' : hours.toString().padStart(2, '0');
+
+    return [paddedHours, paddedMinutes, paddedSeconds].filter(x => x !== '').join(':');
+};
