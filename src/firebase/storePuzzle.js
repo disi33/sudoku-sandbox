1455b347521739a818b66e735a1671062e151520
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2019-05-02 20:38:33 +0100

    Make magic links a touch shorter

--- src/firebase/storePuzzle.js
+++ src/firebase/storePuzzle.js
@@ -2,7 +2,7 @@ import firebase from './firebase';
 
 const generateKey = () => {
     const chars = '2346789bdfghjmnpqrtBDFGHJLMNPQRT';
-    return [...Array(16)].map(_ => chars[Math.floor(Math.random() * chars.length)]).join('');
+    return [...Array(10)].map(_ => chars[Math.floor(Math.random() * chars.length)]).join('');
 }
 
 export const uploadPuzzle = content => firebase.storage().ref(generateKey()).putString(content);
