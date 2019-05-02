c9ecbde37b43383763ccb03ee1f3f069c87c940f
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2019-05-02 20:35:58 +0100

    Use correct firebase configuration

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,11 +3,12 @@ import 'firebase/storage';
 
 const config = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: "pseudoku-app.firebaseapp.com",
-    databaseURL: "https://pseudoku-app.firebaseio.com",
-    projectId: "pseudoku-app",
-    storageBucket: "pseudoku-app.appspot.com",
-    messagingSenderId: "517417182245"
+    authDomain: "sudoku-sandbox.firebaseapp.com",
+    databaseURL: "https://sudoku-sandbox.firebaseio.com",
+    projectId: "sudoku-sandbox",
+    storageBucket: "sudoku-sandbox.appspot.com",
+    messagingSenderId: "698959756837",
+    appId: "1:698959756837:web:32893e73de148887"
 };
 
 export default firebase.initializeApp(config);
