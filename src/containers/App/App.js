30dbbfdd59fe8986f1d6c276f192e60c42d5e9f0
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2019-05-08 09:45:10 +0100

    Added a warning screen to small devices

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -6,6 +6,8 @@ import { loadPuzzle } from '../../actions/saveLoadEditActions';
 import { setInteractionsMode } from '../../actions/puzzleActions';
 import { startPlayOver } from '../../actions/playActions';
 
+import useWindowWidth from '../../hooks/useWindowWidth';
+
 import EditPanel from '../EditPanel/EditPanel';
 import EditPlay from '../EditPlay/EditPlay';
 import PlayPanel from '../PlayPanel/PlayPanel';
@@ -17,6 +19,14 @@ const App = ({isPlay, onPuzzleLoaded}) => {
 
     const [loadingPuzzle, setLoadingPuzzle] = useState(true);
     const key = lastUrlSegment(window.location.href);
+    const width = useWindowWidth();
+
+    if (width < 1008) return (
+        <div className="too-small">
+            <span className="too-small__heading">Sorry!</span>
+            <span className="too-small__body">This site requires a larger screen</span>
+        </div>
+    );
 
     if (loadingPuzzle && key === '') {
         setLoadingPuzzle(false);
